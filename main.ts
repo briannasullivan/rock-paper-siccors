b9a5879fb4980da059f20d51757db8ff72dbc6ca
briannasullivan <113055705+briannasullivan@users.noreply.github.com>
2022-09-20 14:38:46 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,38 @@
+function pickshape () {
+    hand = randint(1, 3)
+    if (true) {
+    	
+    }
+    if (true) {
+    	
+    }
+    if (true) {
+    	
+    }
+}
+let hand = 0
+radio.setGroup(1)
+let rockindex = 1
+let paperindex = 2
+let scissorsindex = 3
+let paper = images.createImage(`
+    # # # # #
+    # . . . #
+    # . . . #
+    # . . . #
+    # # # # #
+    `)
+let rock = images.createImage(`
+    . . . . .
+    . # # # .
+    # # # # #
+    # # # # #
+    . . . . .
+    `)
+let scissors = images.createImage(`
+    # # . . #
+    # # . # .
+    . . # . .
+    # # . # .
+    # # . . #
+    `)
